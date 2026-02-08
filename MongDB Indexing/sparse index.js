0bcd5647c4690100c7d156120aa48e447c1bdb3b
Abdelmagidawad <abdelmagidawad@gmail.com>
2026-02-09 00:17:32 +0200

    use unique index with sparse index

--- MongDB Indexing/sparse index.js
+++ MongDB Indexing/sparse index.js
@@ -24,3 +24,23 @@
 => db.students.createIndex({phone:1},{sparse:true})
 => db.students.find({phone:{$exists:true}})
 => db.students.find({phone:{$exists:true}}).explain("executionStats") // totalDocsExamined: 3
+
+// Use Unique Index with Sparse Index 
+
+=> db.students.insertMany([{name:"Tamer",email:"tamer@gmail.com",age:30},{name:"Mahmoud",email:"mahmoud@gmail.com",age:26}])
+
+=> db.students.createIndex({age:1},{unique:true,sparse:true})
+=> db.students.getIndexes()
+=> db.students.find({age:{$exists:true}})
+=> db.students.find({age:{$exists:true}}).explain("executionStats")
+
+=> db.students.insertOne({name:"Ragab",email:"tamer@gmail.com",age:30}) 
+// => MongoServerError: E11000 duplicate key error collection: indexes.students index: email_1 dup key: { email: "tamer@gmail.com" }
+
+=> db.students.insertOne({name:"Ragab",email:"ragab@gmail.com",age:30}) 
+// => MongoServerError: E11000 duplicate key error collection: indexes.students index: age_1 dup key: { age: 30 }
+
+=> db.students.insertOne({name:"Ragab",email:"ragab@gmail.com",age:25}) 
+=> db.students.find({age:{$exists:true}})
+=> db.students.find({age:{$exists:true}}).count()
+
